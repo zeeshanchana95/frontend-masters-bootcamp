1fc51ab5eed9de54945d7680a0f4dba2b5d36f27
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 08:44:38 +0500

    using booleans

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,21 +1,2 @@
-/**
- * strings:
- *  - bunch of characters sitting next to each other
- *  - used for storing literal values like any word, sentence
- *  - value need to be enclosed inside Single Quotes '', Double Quotes "", or Backticks `` 
- * 
- * Syntax Error: 
- *  - means your grammar is bad, fix it
- *  - pretty easy to fix
- * 
- * String Concatenation:
- *  - connecting strings together
- *  - use + symbol to combine strings together
- */
-
-const firstName = "Zeeshan"
-const lastName = "Ahmed"
-
-const sentence = `Hello ${firstName} ${lastName}! How are you?`;
-
-console.log(sentence);
+const isSkyBlue = true;
+console.log(isSkyBlue); //true
