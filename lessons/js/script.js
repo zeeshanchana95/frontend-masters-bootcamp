340e10b6e81f3e419f7c6990cb990797954f2dfb
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 20:45:15 +0500

    discussing scope in js

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,28 +1,29 @@
-//functions and parameters
+const A = "A";
+let F;
 
-function addTwo(number) {
-return number + 2;
+function doStuff(B) {
+  console.log(B); // works, B parameter is still in scope
+  const C = "C";
+  let H = "H";
+  if (1 + 1 === 2) {
+    const D = "D";
+    H = "something else";
+  }
+  console.log(D); // does not work, D was declared in that if statement block
+  console.log(H); // works, H was declared outside the if statement
+  F = "F";
 }
 
-const finalAnswer = addTwo(5);
-console.log(finalAnswer);
-
-
-function greet(firstName, lastName, honorific, greeting) {
-return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+let E = 0;
+while (E < 3) {
+  E++;
+  console.log(A); // works, the outter block (called the global scope) is still in scope
+  const G = "G";
 }
+console.log(E); // works, E was declared outside the whie loop
+console.log(G); // does not work, declared inside the while loop and it's over
 
-console.log(greet("Brian", "Holt", "Lord", "Salutations"));
-console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
-
-
-
-const myHomeCity = "Salt Lake City";
-const myHomeState = "Utah";
-const myHomeCountry = "USA";
-
-function logOutYourHome(city, state, country) {
-console.log(`You are from ${city}, ${state} ${country}.`);
-}
-
-logOutYourHome(myHomeCity, myHomeState, myHomeCountry);
+doStuff("B");
+console.log(B); // does not work, the B parameter expires after the function call
+console.log(C); // does not work, C was declared inside the function and the function is over
+console.log(F); // works, F was declared in the global scope
