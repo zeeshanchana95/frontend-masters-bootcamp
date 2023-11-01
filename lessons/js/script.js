42a57a159c8018e782cf95c3bad1721bf4449d1c
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 08:47:53 +0500

    js primitive types and number limits

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,2 +1,7 @@
-const isSkyBlue = true;
-console.log(isSkyBlue); //true
+const num = 10;
+const num2 = 10.222;
+const num3 = - 0.5;
+
+console.log(num, num2, num3);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
