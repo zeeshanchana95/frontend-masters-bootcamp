82b71d4264835a669772abe2e37e30a3f10411b4
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 16:41:16 +0500

    using single and multi-line comments in js

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -3,6 +3,16 @@
 let friendsAtYourParty = 0;
 
 while(friendsAtYourParty <= 10) {
+
+    // this is a single line comment in js
     friendsAtYourParty = friendsAtYourParty + 1;
+
+    /*
+
+    this is 
+    
+    multi-line comment in js
+
+    */
 }
 console.log(friendsAtYourParty); //10
