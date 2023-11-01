87490ec28c48b92761587876419b7ad31fe90365
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 20:55:06 +0500

    scope example

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,29 +1,19 @@
-const A = "A";
-let F;
+//scope example
 
-function doStuff(B) {
-  console.log(B); // works, B parameter is still in scope
-  const C = "C";
-  let H = "H";
-  if (1 + 1 === 2) {
-    const D = "D";
-    H = "something else";
-  }
-  console.log(D); // does not work, D was declared in that if statement block
-  console.log(H); // works, H was declared outside the if statement
-  F = "F";
+let something = 'outside the function';
+let thingToKeep;
+
+function thing() {
+    let something = 'outside the if statement';
+    if(true) {
+        let something = 'inside the if statement';
+        console.log(something)
+    }
+    console.log(something);
+    thingToKeep = 'defined now';
 }
 
-let E = 0;
-while (E < 3) {
-  E++;
-  console.log(A); // works, the outter block (called the global scope) is still in scope
-  const G = "G";
-}
-console.log(E); // works, E was declared outside the whie loop
-console.log(G); // does not work, declared inside the while loop and it's over
+thing();
 
-doStuff("B");
-console.log(B); // does not work, the B parameter expires after the function call
-console.log(C); // does not work, C was declared inside the function and the function is over
-console.log(F); // works, F was declared in the global scope
+console.log(something);
+console.log(thingToKeep);
