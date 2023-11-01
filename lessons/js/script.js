d2e7b95317e69f4dfcf9cf108eba18c16db31430
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 10:07:38 +0500

    using expressions inside if() and talk about =, ==, ===

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,21 +1,10 @@
-/**
- * Control Flow: 
- * - if-else statements
- * - ask questions inside if(question) and question has to be true or false
- * - block: whatever inside {} and ends when bracket ends } ends
- * - helps do conditional logic
- * 
- */
 
-
-//like to make boolean questions, get the idea it is boolean
-const isSkyBlue = true;
-
-let greeting;
-
-if(isSkyBlue) {
-    greeting = "It must be nice weather"
-    //execute if true
+if(2 + 2 === 4) {
+ // === means is equal to 
+ console.log("Horray! Math still works")
 } else {
-   greeting = "It must be not nice weather";//executes if false
+    console.log("Uh, panic?")
 }
+
+//why ===, == still works
+// == don't want to do becuase of cooersion 2 + 2 == "4" -> true but if 2 + 2 === "4" -> false
