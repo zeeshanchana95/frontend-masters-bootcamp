9032af6a64ca3f5f9f8879e24f856cdface9e6d9
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 07:36:06 +0500

    passing multiple parameters to console.log()

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -3,7 +3,7 @@
 const montlyRent = 500;
 
 let yearlyRent = montlyRent * 12;
-console.log(yearlyRent);  //6000
+console.log(montlyRent, yearlyRent);  //500, 6000
 
 yearlyRent = 1;
 console.log(yearlyRent); //1
