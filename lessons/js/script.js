73b20d908d896554df0b613e3f5b4a3de34e3a2b
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 16:37:14 +0500

    using while loop and creating infinite loop

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,11 +1,8 @@
-//if-elseif-else statements
+//loops-while loop-infinite loop
 
 let friendsAtYourParty = 0;
 
-if(friendsAtYourParty === 0) {
-    console.log("Woo, now all the things is yours");
-} else if(friendsAtYourParty >= 4) {
-    console.log("That's good number for game");
-} else {
-    console.log("Let's dance")
+while(friendsAtYourParty <= 10) {
+    friendsAtYourParty = friendsAtYourParty + 1;
 }
+console.log(friendsAtYourParty); //10
