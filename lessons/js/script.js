7b4875a72ff92643116653844b5ab31833ee4c0c
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-11-01 16:45:52 +0500

    different ways of increementing variables in js

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,18 +1,16 @@
-//loops-while loop-infinite loop
+//ways of increementing variables by one or by different value
 
 let friendsAtYourParty = 0;
 
 while(friendsAtYourParty <= 10) {
 
-    // this is a single line comment in js
+    //all the statements below are used for increementing the variable by 1;
     friendsAtYourParty = friendsAtYourParty + 1;
+    friendsAtYourParty += 1;
+    friendsAtYourParty++;
+    ++friendsAtYourParty;
 
-    /*
 
-    this is 
-    
-    multi-line comment in js
-
-    */
+    console.log(friendsAtYourParty)
 }
 console.log(friendsAtYourParty); //10
